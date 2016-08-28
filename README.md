5c5c3a7e51000c9fb6f7f45e028dc2594b77ddf3
Harry <harry@boojum.com.au>
2016-08-28 13:37:43 -0400

    initialised

---
+++ README.md
@@ -0,0 +1,12 @@
+Install:
+fill out name and description in package.json
+npm install
+
+Develop:
+npm start
+localhost:3000
+
+Build for production:
+npm run prod
+
+Then host the public/ directory (eg. drag it into netlify)
---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+var debug = process.env.NODE_ENV !== "production"
+var webpack = require('webpack')
+var path = require('path')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+
+
+module.exports = {
+  entry: debug ? [
+    'babel-polyfill',
+    'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+    './src/index.js'
+  ] : [
+    'babel-polyfill',
+    './src/index.js'
+  ],
+
+  output: {
+    path: path.join(__dirname, 'public/static'),
+    filename: 'bundle.js',
+    contentBase: path.join(__dirname, 'public'),
+    publicPath: '/static/'
+  },
+
+  devtool: debug ? 'cheap-module-eval-source-map' : 'cheap-module-source-map',
+
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        loaders: debug ? ['react-hot','babel']:['babel'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.(woff(2)?|png|jpg|gif)(\?[a-z0-9=\.]+)?$/,
+        loader: 'url-loader',
+        query: {
+          limit: '100000'
+        }
+      },
+      [
+        {// global
+          test: /\.css$/,
+          exclude: path.join(__dirname, 'src/containers'),
+          loader: debug ? 'style-loader!css-loader!postcss-loader' : ExtractTextPlugin.extract('style-loader','css-loader!postcss-loader')
+        },
+        {// else local
+          test: /\.css$/,
+          loader: debug ? 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader' : ExtractTextPlugin.extract('style-loader','css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader')
+        },
+      ],
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
+      }
+    ]
+  },
+
+  postcss: [
+    require('autoprefixer')
+  ],
+
+  plugins: debug
+    ? [
+      new webpack.HotModuleReplacementPlugin(),
+    ]
+    : [
+      new ExtractTextPlugin('style.css'),
+      new webpack.DefinePlugin({
+        'process.env':{
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false, compress: {warnings: true }})
+    ]
+
+}
