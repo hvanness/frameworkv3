4459c9338e3aea07b768aec30998b7f5a52b9f0e
Harry <harry@boojum.com.au>
2017-07-29 02:03:32 +1000

    fixed import

--- src/app/index.js
+++ src/app/index.js
@@ -3,7 +3,7 @@ import Helmet from 'react-helmet'
 import { Link } from 'react-router-dom'
 import { Route } from 'react-router'
 import 'normalize.css'
-import Example from './example/containers'
+import Example from './containers/example'
 
 export default class App extends Component {
   render() {
